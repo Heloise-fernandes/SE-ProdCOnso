ce96c24c9377fbfaa352e7ce34466675fa6079b3
Heloise-fernandes <heloies.fernandesdealmeida@gmail.com>
2016-11-24 16:43:34 +0100

    blop

--- src/prodcons/v1/Producteur.java
+++ src/prodcons/v1/Producteur.java
@@ -1,6 +1,9 @@
 package prodcons.v1;
 
+import java.util.Random;
+
 import jus.poc.prodcons.Acteur;
+import jus.poc.prodcons.Aleatoire;
 import jus.poc.prodcons.ControlException;
 import jus.poc.prodcons.Observateur;
 import jus.poc.prodcons._Producteur;
@@ -9,8 +12,11 @@ public class Producteur extends Acteur implements _Producteur{
 	
 	int nbMessage;
 
-	protected Producteur( Observateur observateur, int moyenneTempsDeTraitement,int deviationTempsDeTraitement) throws ControlException {
+	protected Producteur( Observateur observateur, int moyenneTempsDeTraitement,int deviationTempsDeTraitement, int nbMoyenProducteur, int derivationProd) throws ControlException {
 		super(Acteur.typeProducteur, observateur, moyenneTempsDeTraitement, deviationTempsDeTraitement);
+		//TODO : Generer nbMessage à produire
+		Aleatoire alea = new Aleatoire(nbMoyenProducteur, derivationProd);
+		this.nbMessage = alea.next();
 		
 	}
 

3b80953277f6db103fd1e82a74fd5d81cefa3c86
Maxime <maxime.chevalier@outlook.com>
2016-11-24 15:01:49 +0100

    Implement les bonnes choses

--- src/prodcons/v1/ProdCons.java
+++ src/prodcons/v1/ProdCons.java
@@ -1,3 +1,4 @@
+package prodcons.v1;
 
 public class ProdCons {
 
---
+++ src/prodcons/v1/TestProdCons.java
@@ -0,0 +1,19 @@
+package prodcons.v1;
+
+import jus.poc.prodcons.Observateur;
+import jus.poc.prodcons.Simulateur;
+
+public class TestProdCons extends Simulateur {
+
+	public TestProdCons(Observateur observateur) {
+		super(observateur);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void run() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
